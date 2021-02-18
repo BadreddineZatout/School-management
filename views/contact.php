840103cf45d3a6702d506d9f64e6c98e760c3eb5
BadreddineZatout <hb_zatout@esi.dz>
2021-02-18 09:14:34 +0100

    contact page layout

--- views/contact.php
+++ views/contact.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once 'includes/link.php' ?>
+    <link rel="stylesheet" href="style/contact.css">
     <title>Contact</title>
 </head>
 <body>
@@ -12,7 +13,22 @@
         require_once 'includes/header.php';
         require_once 'includes/menu.php';
     ?>
-
+    <div class="col-sm-10 card mx-auto">
+        <div class="card-header">
+            <h3>Pour nous contacter, veuillez consulter les informatins suivantes : </h3>
+        </div>
+        <div class="card-body row p-5">
+            <div class="col-sm-12 mb-5">
+                <p><i class="fas fa-map-pin fa-3x"></i> <b>Adresse ecole</b></p>
+            </div>
+            <div class="col-sm-12 mb-5">
+                <p><i class="fas fa-phone fa-2x"></i> <b>065720706</b></p>
+            </div>
+            <div class="col-sm-12">
+                <p><i class="fas fa-tty fa-2x"></i> <b>0797498940</b></p>
+            </div>
+        </div>
+    </div>
     <?php
         require_once 'includes/footer.php'
     ?>
