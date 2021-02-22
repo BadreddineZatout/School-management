b15e58c9ecbf95754ac517b96b21815820d35726
BadreddineZatout <hb_zatout@esi.dz>
2021-02-22 11:09:28 +0100

    changed layout and added back-end to presentation page

--- views/ecole.php
+++ views/ecole.php
@@ -23,18 +23,26 @@
             <div class='card-header'>
                 <h1>Nom ecole</h1>
             </div>
-            <div class="card-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-        </div>
-
-        <div class="col-sm-5 ppt mx-auto my-5">
-            <div class="card-deck p-3">
-                <div class="card">
-                    <img src="../data/images/img.jpg" alt="image">
+            <?php
+                foreach ($infos as $info) {
+                    ?>
+            <div class="card-body row ppt mx-1 my-1">
+                <div class="col">
+                    <p><?= $info['paragraphe'] ?></p>
+                </div>
+                <?php
+                    if (!is_null($info['image'])) {
+                        ?>
+                <div class="col-4 mr-auto">
+                    <img src="<?= $info['image'] ?>" height="300px" width="400px" alt="">
                 </div>
             </div>
+            <?php
+                    }
+                } 
+            ?>
         </div>
+        
     </div>
     <?php
         require_once 'includes/footer.php'
