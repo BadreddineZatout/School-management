43280e90d537642f27c95eba7b2cfa664879b68d
BadreddineZatout <hb_zatout@esi.dz>
2021-02-22 13:12:03 +0100

    get primary articles

--- controllers/PrimaireController.php
+++ controllers/PrimaireController.php
@@ -1,13 +1,19 @@
 <?php
+include 'ArticleController.php';
 class PrimaireController{
-
-
     public function primaire_page()
     {
         $pc = $this;
+        $articles = $this->get_articles();
         require_once 'views/primaire.php';
     }
     public function get_cadres(){
         require_once 'views/cycle/cycle-cadre.php';
+    }
+    public function get_articles(){
+        $ac = new ArticlesController();
+        $articles = $ac->get_articles_AP();
+        $articles = $ac->div_article($articles);
+        return $articles;
     }
 }
