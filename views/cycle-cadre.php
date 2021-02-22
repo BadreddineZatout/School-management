a40b02c1221c78fecc07bd7476b26b58b0cad41b
BadreddineZatout <hb_zatout@esi.dz>
2021-02-22 11:43:39 +0100

    mettre les cadre de cycles dans un fichier php separer pour les inclure

---
+++ views/cycle-cadre.php
@@ -0,0 +1,34 @@
+<div class="col-12 card-deck">
+    <div class="card">
+        <img class="card-img-top" src="../data/images/edt.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Emploi de temps</h4>
+            <p class="card-text">Consulter l'emploi du temps</p>
+            <a class="btn" href="#">Afficher la suite</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/ens.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Enseignants</h4>
+            <p class="card-text">Consulter la liste des enseignants et leur heure de reception</p>
+            <a class="btn" href="#">Afficher la suite</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/info-pratique.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Inforamtions Pratiques</h4>
+            <p class="card-text">Consulter les informations pratiques</p>
+            <a class="btn" href="#">Afficher la suite</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/restau.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Restauration</h4>
+            <p class="card-text">Consulter le planning de restauration</p>
+            <a class="btn" href="#">Afficher la suite</a>
+        </div>
+    </div>
+</div>
