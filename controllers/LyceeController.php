b5ea5a41ef203ee9d087232a6a204a9eac45b382
BadreddineZatout <hb_zatout@esi.dz>
2021-02-22 11:43:04 +0100

    created CEMCntroller class and added get_cadres function

--- controllers/LyceeController.php
+++ controllers/LyceeController.php
@@ -1,6 +1,12 @@
 <?php
+class LyceeController{
 
-function lycee_page()
-{
-    require_once 'views/lycee.php';
+    function lycee_page()
+    {
+        $lc = $this;
+        require_once 'views/lycee.php';
+    }
+    public function get_cadres(){
+        require_once 'views/cycle-cadre.php';
+    }
 }
