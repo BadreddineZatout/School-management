fa342a09e3b0836fca3ec2ee55864dde5c519771
BadreddineZatout <hb_zatout@esi.dz>
2021-02-25 12:39:23 +0100

    eleves scriptes

---
+++ includes/eleve_script.php
@@ -0,0 +1,5 @@
+<script src="../js/jquery-3.5.1.js"></script>
+<script src="../js/note.js"></script>
+<script src="../js/activite.js"></script>
+<script src="../js/remarque.js"></script>
+<script src="../js/eleve.js"></script>
