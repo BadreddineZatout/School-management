0c9e739adf303489309ba58df65cd66cec567be0
BadreddineZatout <hb_zatout@esi.dz>
2021-02-25 09:59:43 +0100

    show and hide tabls in eleve page

---
+++ js/eleve.js
@@ -0,0 +1,29 @@
+$('document').ready(function(){
+    $('#notes').hide();
+    $('#activites').hide();
+    $('#remarques').hide();
+});
+$('#edt-btn').click(function () { 
+    $('#notes').hide();
+    $('#activites').hide();
+    $('#remarques').hide();
+    $('#edt').show();
+});
+$('#note-btn').click(function () { 
+    $('#activites').hide();
+    $('#remarques').hide();
+    $('#edt').hide();
+    $('#notes').show();
+});
+$('#activ-btn').click(function () { 
+    $('#notes').hide();
+    $('#remarques').hide();
+    $('#edt').hide();
+    $('#activites').show();
+});
+$('#rque-btn').click(function () { 
+    $('#notes').hide();
+    $('#activites').hide();
+    $('#edt').hide();
+    $('#remarques').show();
+});
