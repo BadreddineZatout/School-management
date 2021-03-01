fff7003d2cf7cdb0233d952dcbd38fffd67f2872
BadreddineZatout <hb_zatout@esi.dz>
2021-03-01 21:43:01 +0100

    admin categoties

--- views/admin.php
+++ views/admin.php
@@ -13,6 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once 'includes/link.php' ?>
+    <link rel="stylesheet" href="style/articles.css">
     <title>Admin</title>
 </head>
 <body>
@@ -20,7 +21,75 @@
         require_once 'includes/header.php';
         require_once 'includes/menu.php';
     ?>
-
+<div class="col-12 card-deck">
+    <div class="card">
+        <img class="card-img-top" src="../data/images/articles.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Les Articles</h4>
+            <p class="card-text">Gerer les articles de l'ecole</p>
+            <a class="btn" href="/?action=edt&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/ppt.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Presentaion de l'Ecole</h4>
+            <p class="card-text">Gerer le contenu de la presentation de l'ecole</p>
+            <a class="btn" href="/?action=ens&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/edt.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Emploi de temps</h4>
+            <p class="card-text">Gerer l'emploi du temps de chaque cycle</p>
+            <a class="btn" href="/?action=info-pratique&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/ens.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Les Enseignants</h4>
+            <p class="card-text">Geres les enseignants de chaque cycle</p>
+            <a class="btn" href="/?action=restau&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+</div>
+<div class="col-12 card-deck">
+    <div class="card">
+        <img class="card-img-top" src="../data/images/users.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Les Utilisateurs</h4>
+            <p class="card-text">Geres les enseignants de site</p>
+            <a class="btn" href="/?action=edt&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/restau.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Restauration</h4>
+            <p class="card-text">Gerer le planning de Restauration de chaque cycle</p>
+            <a class="btn" href="/?action=restau&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/contact.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Contact</h4>
+            <p class="card-text">Gerer les information dans la page du contact</p>
+            <a class="btn" href="/?action=ens&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="../data/images/info-pratique.jpg" alt="card image">
+        <div class="card-body">
+            <h4 class="card-title">Parametres</h4>
+            <p class="card-text">Gerer les parametres de site</p>
+            <a class="btn" href="/?action=info-pratique&cycle=<?= $cycle ?>">Aller au page</a>
+        </div>
+    </div>
+    
+</div>
     <?php
         require_once 'includes/footer.php'
     ?>
