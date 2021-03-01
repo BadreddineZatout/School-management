7f016d82f677225e2c38252cc9dcf0531ce27b3a
BadreddineZatout <hb_zatout@esi.dz>
2021-03-01 22:12:41 +0100

    pages de gestion

---
+++ views/admin/ppt.php
@@ -0,0 +1,25 @@
+<?php
+    if(!isset($_GET['action'])){
+        header('location:/');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'includes/link.php' ?>
+    <title>Gestion de presentation de l'ecole</title>
+</head>
+<body>
+    <?php 
+        require_once 'includes/header.php';
+        require_once 'includes/menu.php';
+    ?>
+    
+    <?php
+        require_once 'includes/footer.php'
+    ?>
+</body>
+</html>
