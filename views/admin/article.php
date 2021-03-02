08bb614929f0d969b4c372ed5d563e4a3288d81f
BadreddineZatout <hb_zatout@esi.dz>
2021-03-02 18:21:12 +0100

    add modal

--- views/admin/article.php
+++ views/admin/article.php
@@ -18,12 +18,45 @@
         require_once 'includes/header.php';
         require_once 'includes/menu.php';
     ?>
+    <div class="modal" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Ajouter Un Article</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
+          <!-- Modal body -->
+        <form action="" method="post">
+          <div class="modal-body">
+                <div class="form-group">
+                  <label for="email">Email address:</label>
+                  <input type="email" class="form-control" placeholder="Enter email" id="email">
+                </div>
+                <div class="form-group">
+                  <label for="pwd">Password:</label>
+                  <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+                </div>
+            </div>
+            
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+        </form>
+
+        </div>
+      </div>
+    </div>
     <div class="d-flex head p-2 mx-5 mb-3">
         <span class="mr-auto">
             <h1>Les Articles</h1>
         </span>
         <span class="my-auto">
-            <button class="btn b">Ajouter un article</button>
+            <button class="btn b" data-toggle="modal" data-target="#myModal">Ajouter un article</button>
         </span>
     </div>
     <div class="mx-5">
@@ -43,6 +76,7 @@
     ?>
 </body>
 <script src="js/jquery-3.5.1.js"></script>
+<script src="js/bootstrap.js"></script>
 <script src="js/admin/buttons.js"></script>
 <script src="js/admin/article.js"></script>
 </html>
