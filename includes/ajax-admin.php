b6dfda92ee08e3d300b87ad038e1a47acd2cdc65
BadreddineZatout <hb_zatout@esi.dz>
2021-03-02 18:20:48 +0100

    articles crud routes

--- includes/ajax-admin.php
+++ includes/ajax-admin.php
@@ -5,4 +5,19 @@ switch ($_GET['action']) {
         $ac = new ArticlesController();
         echo($ac->getAll());
         break;
+    case 'storeArticle': 
+        require_once('./controllers/ArticleController.php');
+        $ac = new ArticlesController();
+        $ac->store();
+        break;
+    case 'updateArticle': 
+        require_once('./controllers/ArticleController.php');
+        $ac = new ArticlesController();
+        $ac->update();
+        break;
+    case 'deleteArticle': 
+        require_once('./controllers/ArticleController.php');
+        $ac = new ArticlesController();
+        $ac->delete();
+        break;
     }
