31bea4c8e5fc68874bc2e1c7859d3b232ecc0b4a
BadreddineZatout <hb_zatout@esi.dz>
2021-03-06 15:27:23 +0100

    fix error

--- models/Enseignant.php
+++ models/Enseignant.php
@@ -44,9 +44,9 @@ class Enseignant extends Model{
         if(!$exist){
             $query = $this->db->prepare("INSERT INTO enseignants_class (ens_id, class_id) VALUES (?, ?)");
             $query->execute([$_POST['enseignant'], $_POST['class']]);
-            $query = $this->db->prepare("INSERT INTO enseignants_heure (ens_id, class_id, heure) VALUES (?, ?, ?)");
-            $query->execute([$_POST['enseignant'], $_POST['class'], $_POST['heure']]);
         }
+        $query = $this->db->prepare("INSERT INTO enseignants_heure (ens_id, class_id, heure) VALUES (?, ?, ?)");
+        $query->execute([$_POST['enseignant'], $_POST['class'], $_POST['heure']]);
         if(!empty($_POST['recep'])){
             $query = $this->db->prepare("UPDATE enseignants SET temps_recep=?WHERE id=?");
             $query->execute([$_POST['recep'], $_POST['enseignant']]);
